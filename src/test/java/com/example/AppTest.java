7971a9dcd32609e84a230a132e387ece832936a7
Andy Laffertt <alaffertt@gmail.com>
2025-05-17 15:31:12 -0400

    requerimiento 1 completo

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -10,16 +10,46 @@ public class AppTest {
         TicTacToe juego = new TicTacToe();
         try {
             juego.insertarEn(-1, 1);
-            fail("Se esperaba IllegalArgumentException");
+            fail("Se esperaba IllegalArgumentException por fila incorrecta");
         } catch (IllegalArgumentException e) {
-            // OK
+            System.out.println("Excepción capturada correctamente fila -1");
         }
 
         try {
             juego.insertarEn(3, 1);
             fail("Se esperaba IllegalArgumentException");
         } catch (IllegalArgumentException e) {
-            // OK
+            System.out.println("Excepción capturada correctamente fila 3");
         }
     }
+    @Test
+public void testPiezaFueraDelEjeY_lanzaExcepcion() {
+    TicTacToe juego = new TicTacToe();
+
+    try {
+        juego.insertarEn(1, -1);
+        fail("Se esperaba IllegalArgumentException para columna -1");
+    } catch (IllegalArgumentException e) {
+        System.out.println("Excepción capturada correctamente para columna -1");}
+
+    try {
+        juego.insertarEn(1, 3);
+        fail("Se esperaba IllegalArgumentException para columna 3");
+    } catch (IllegalArgumentException e) {
+        System.out.println("Excepción capturada correctamente para columna 3");
+    }
+}
+@Test
+public void testPiezaEnLugarOcupado_lanzaExcepcion() {
+    TicTacToe juego = new TicTacToe();
+    juego.insertarEn(0, 0); // Jugador X
+
+    try {
+        juego.insertarEn(0, 0); // Mismo lugar
+        fail("Se esperaba IllegalStateException por posición ocupada");
+    } catch (IllegalStateException e) {
+        System.out.println("Excepción capturada correctamente para posición ocupada");
+    }
+}
+
 }
